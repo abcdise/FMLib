692238c9959fb494354ebb7253ca2bb8082a1a0b
abcdise <zelin.r.ad@mein.gmx>
2023-09-05 22:20:34 +0200

    Commit 2023.09.05

--- Portfolio.cpp
+++ Portfolio.cpp
@@ -11,31 +11,35 @@ public:
     vector<double> quantities;
     vector<shared_ptr<Priceable>> securities;
 };
-
-shared_ptr<Portfolio> Portfolio::newInstance(){
-    shared_ptr<Portfolio> ret = make_shared<PortfolioImpl>();
-    return ret;
-}
+ 
 
 int PortfolioImpl::size() const {
-    return int(securities.size());
+    return quantities.size();
 }
-
-int PortfolioImpl::add(double quantity, shared_ptr<Priceable> security) {
-    securities.push_back(security);
-    quantities.push_back(quantity);
-    return int(securities.size());
+ 
+int PortfolioImpl::add( double quantity,
+            shared_ptr<Priceable> security ) {
+    quantities.push_back( quantity );
+    securities.push_back( security );
+    return quantities.size();
 }
-
-void PortfolioImpl::setQuantity(int index, double quantity) {
+ 
+double PortfolioImpl::price(
+        const BlackScholesModel& model ) const {
+    double ret = 0;
+    int n = size();
+    for (int i=0; i<n; i++) {
+        ret += quantities[i]
+               * securities[i]->price( model );
+    }
+    return ret;
+}
+void PortfolioImpl::setQuantity( int index,
+        double quantity ) {
     quantities[index] = quantity;
 }
 
-double PortfolioImpl::price(const BlackScholesModel& model) const {
-    double ret = 0;
-    auto n = size();
-    for (auto i=0; i<n; i++){
-        ret += quantities[i] * securities[i]->price(model);
-    }
+shared_ptr<Portfolio> Portfolio::newInstance() {
+    shared_ptr<Portfolio> ret = make_shared<PortfolioImpl>();
     return ret;
 }
--- Portfolio.h
+++ Portfolio.h
@@ -2,21 +2,19 @@
 
 #include "Priceable.h"
 
-class Portfolio: public Priceable {
+class Portfolio : public Priceable {
 public:
-    /* Virtual destructor */
-    virtual ~Portfolio(){};
-    /* Number of items in the portfolio */
+    /*  Virtual destructor */
+    virtual ~Portfolio() {};
+    /* Returns the number of items in the portfolio*/
     virtual int size() const = 0;
-    /* 
-    Add a new security to the portfolio and return the index at which 
-    it is added
-    */
+    /*  Add a new security to the portfolio,
+        returns the index at which it was added */
     virtual int add(double quantity, std::shared_ptr<Priceable> security) = 0;
-    /* Set the quantity of a certain security */
+    /*  Update the quantity at a given index */
     virtual void setQuantity(int index, double quantity) = 0;
-    /* Compute the current price */
-    virtual double price(const BlackScholesModel& model) = 0;
-    /* Create a portfolio */
+    /*  Compute the current price */
+    virtual double price(const BlackScholesModel& model) const = 0;
+    /*  Creates a Portfolio */
     static std::shared_ptr<Portfolio> newInstance();
 };
